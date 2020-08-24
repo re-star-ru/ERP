778d7eac265e9736106bf1ed7db543a5cbce2a16
fess932 <fess932@gmail.com>
2020-08-24 09:03:28 +0300

    testing

---
+++ backend/internal/app/product/repository/testrepo/testrepo.go
@@ -0,0 +1,33 @@
+package testrepo
+
+import (
+	"backend/internal/app/models"
+	"backend/internal/app/product"
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type repository struct {
+	db *mongo.Collection
+}
+
+func (r repository) GetByID(ctx context.Context, id string) (models.Product, error) {
+	panic("implement me")
+}
+
+func (r repository) GetProducts(ctx context.Context) ([]models.Product, error) {
+	panic("implement me")
+}
+
+func (r repository) CreateProduct(ctx context.Context, user *models.User, product models.Product) error {
+	panic("implement me")
+}
+
+func (r repository) UpdateProduct(ctx context.Context, user *models.User, product models.Product) error {
+	panic("implement me")
+}
+
+func NewRepository(collection string) product.Repository {
+	return &repository{DB.Collection(collection)}
+}
