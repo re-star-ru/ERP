cf63271cd0ae152d81b971d2b29d412f31c6519a
Your Name <fess932@gmail.com>
2022-04-02 16:56:07 +0300

    add openapi for onec

--- backend/cmd/proxy/main.go
+++ backend/cmd/proxy/main.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/rs/zerolog"
-	log "github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
--- backend/pkg/item/repo/repo_1c.go
+++ backend/pkg/item/repo/repo_1c.go
@@ -49,31 +49,40 @@ func (c *ClientOnec) Items(offset, limit int) (map[string]item.Item, error) {
 }
 
 func (c *ClientOnec) TextSearch(s string) (map[string]item.Item, error) {
-	r, err := http.NewRequest("GET", c.Host+"products/batch", nil)
+	w, err := c.newRequest("GET", c.Host+"products/"+s)
 	if err != nil {
-		return nil, fmt.Errorf("cant create new products barch request %w", err)
+		return nil, err
 	}
-	r.Header.Set("Authorization", "Basic "+c.Token)
 
-	resp, err := c.hc.Do(r)
+	r, err := c.hc.Do(w)
 	if err != nil {
 		return nil, fmt.Errorf("cant do request %w", err)
 	}
 	defer r.Body.Close()
 
-	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
-		body, err := io.ReadAll(resp.Body)
+	if r.StatusCode < 200 || r.StatusCode >= 400 {
+		body, err := io.ReadAll(r.Body)
 		if err != nil {
 			return nil, fmt.Errorf("cant read body: %w", err)
 		}
 
-		return nil, fmt.Errorf("error: %s : %s : %d", body, resp.Status, resp.StatusCode)
+		return nil, fmt.Errorf("error: %s : %s : %d", body, r.Status, r.StatusCode)
 	}
 
 	m := map[string]item.Item{}
-	if err := json.NewDecoder(resp.Body).Decode(&m); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
 		return nil, fmt.Errorf("cannot decode body to products %w", err)
 	}
 
 	return m, nil
+}
+
+func (c *ClientOnec) newRequest(method, path string) (*http.Request, error) {
+	w, err := http.NewRequest(method, path, nil)
+	if err != nil {
+		return nil, fmt.Errorf("cant create new products barch request %w", err)
+	}
+	w.Header.Set("Authorization", "Basic "+c.Token)
+
+	return nil, err
 }
