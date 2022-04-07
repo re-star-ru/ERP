d57859a1aea9ce8807c3ed420614d14b2ef4bc0e
Ivan Cheremisin <fess932@gmail.com>
2022-04-07 11:01:02 +0300

    fix put image

--- backend/pkg/img/delivery.go
+++ backend/pkg/img/delivery.go
@@ -1,10 +1,14 @@
 package img
 
 import (
+	"backend/pkg"
 	"context"
+	"errors"
+	"io"
 	"net/http"
 	"path"
 
+	"github.com/go-chi/render"
 	"github.com/google/uuid"
 	"github.com/minio/minio-go/v7"
 	"github.com/rs/zerolog/log"
@@ -37,23 +41,29 @@ type ImageService struct {
 	srv1cbucket string
 }
 
+// PutImage put image to s3 and return path bucket/key
 func (s *ImageService) PutImage(w http.ResponseWriter, r *http.Request) {
+	if r.ContentLength < 100 {
+		pkg.SendErrorJSON(w, r, http.StatusBadRequest, errors.New("not an image"), "wrong content length")
+		return
+	}
+
 	filePath := path.Join("images", uuid.NewString()+".jpeg")
-	log.Debug().Msg(filePath)
 	info, err := s.m.PutObject(
 		context.Background(), s.srv1cbucket, filePath, r.Body, -1,
 		minio.PutObjectOptions{ContentType: r.Header.Get("Content-Type")},
 	)
 
 	if err != nil {
-		log.Err(err).Msg("ошибка загрузки изображения")
+		pkg.SendErrorJSON(w, r, http.StatusInternalServerError, err, "error image upload")
+		return
 	}
 
-	log.Printf("путь к файлу: %s/%s", info.Bucket, info.Key)
-
-	w.Write([]byte(path.Join(info.Bucket, info.Key)))
+	render.Status(r, http.StatusCreated)
+	io.WriteString(w, path.Join(info.Bucket, info.Key))
 }
 
 func (s *ImageService) DeleteImage(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("пока не реализованно"))
+	render.Status(r, http.StatusInternalServerError)
+	io.WriteString(w, "пока не реализованно")
 }
