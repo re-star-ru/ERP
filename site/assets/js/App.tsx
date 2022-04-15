9ab7674b10f4765478ce2713284b1c5d7f214ced
Ivan Cheremisin <fess932@gmail.com>
2022-04-15 10:58:06 +0300

    update app

--- site/assets/js/App.tsx
+++ site/assets/js/App.tsx
@@ -1,10 +1,5 @@
-// import React from "react" // import * as React from "react"
-// import Tran from "./components/Tran"
-
 import React, { useState } from "react"
 import { Filters } from "./components/Filters"
-
-// import Card from "./components/Card"
 
 let arr: Item[] = []
 if (process.env.NODE_ENV !== "production") {
@@ -24,6 +19,7 @@ function App(): JSX.Element {
   const [text, setText] = useState<string>("")
   const [founded, setFounded] = useState<Item[]>(arr)
   const [loading, setLoading] = useState(false)
+  const [searchAlert, setSearchAlert] = useState(false)
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setText(e.target.value)
@@ -35,11 +31,25 @@ function App(): JSX.Element {
     }
   }
 
+  function showSearchAlert() {
+    setSearchAlert(true)
+    setTimeout(() => {
+      setSearchAlert(false)
+    }, 3000)
+  }
+
   async function searchHandler() {
     setLoading(true)
-    const resp = await sendRequest(text)
-    console.log("resp:", resp)
-    setFounded(resp)
+
+    try {
+      const resp = await sendRequest(text)
+      console.log("resp:", resp)
+      setFounded(resp)
+    } catch (e) {
+      showSearchAlert()
+      console.error("Error response")
+    }
+
     setLoading(false)
   }
 
@@ -85,15 +95,18 @@ function App(): JSX.Element {
         </button>
       </div>
 
+      {searchAlert && (
+        <div className='absolute inset-x-0 mx-auto max-w-xl'>
+          <SearchAlert />
+        </div>
+      )}
+
       <div className='container  mx-auto max-w-3xl px-4 pb-2 border-b  border-gray-200'>
         <Filters />
       </div>
-
       <div>
         <CardList cards={founded} />
       </div>
-
-      {/* <Tran /> */}
     </div>
   )
 }
@@ -118,6 +131,31 @@ type Item = {
 
 type CardListProps = {
   cards: Item[]
+}
+
+function SearchAlert() {
+  return (
+    <div
+      className='flex p-4 mb-4 text-sm text-yellow-700 bg-yellow-100 rounded-lg dark:bg-yellow-200 dark:text-yellow-800'
+      role='alert'
+    >
+      <svg
+        className='inline flex-shrink-0 mr-3 w-5 h-5'
+        fill='currentColor'
+        viewBox='0 0 20 20'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          fillRule='evenodd'
+          d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z'
+          clipRule='evenodd'
+        ></path>
+      </svg>
+      <div>
+        <span className='font-medium'>Ошибка на сервере!</span>
+      </div>
+    </div>
+  )
 }
 
 function CardList(props: CardListProps) {
