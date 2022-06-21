c2defed006b38be193b647d4e6ddb7ce9417de24
Ivan Cheremisin <fess932@gmail.com>
2022-06-21 15:53:34 +0300

    work at ent

--- backend/ent/start/start.go
+++ backend/ent/start/start.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -49,4 +50,32 @@ func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
 
 	log.Printf("queried user: %v", u)
 	return u, nil
+}
+
+func CreateCar(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	tesla, err := client.Car.Create().SetModel("Tesla").SetRegisteredAt(time.Now()).Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create car: %w", err)
+	}
+
+	log.Printf("created car: %v", tesla)
+
+	// Create a new car with model "Ford".
+	ford, err := client.Car.
+		Create().
+		SetModel("Ford").
+		SetRegisteredAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating car: %w", err)
+	}
+	log.Println("car was created: ", ford)
+
+	a8m, err := client.User.Create().SetName("a8m").SetAge(20).AddCars(tesla, ford).Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", a8m)
+
+	return a8m, nil
 }
