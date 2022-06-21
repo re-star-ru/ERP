b8d7e5a977d21d3a0cff39854a76464133ce257f
Ivan Cheremisin <fess932@gmail.com>
2022-06-21 11:23:23 +0300

    [no ci] http skeleton

---
+++ backend/pkg/restaritem/delivery/restaritem_http.go
@@ -0,0 +1,47 @@
+package delivery
+
+import (
+	"backend/pkg/restaritem"
+	"net/http"
+)
+
+// будет json ручка
+// адрес для заполнения данных по товару
+// адрес для выкладывания товара на сайт
+// список всех товаров на сайте так же
+
+type IHTTPRestaritemUsecase interface {
+	Create(restaritem *restaritem.RestarItem) (*restaritem.RestarItem, error)
+	GetAll() ([]restaritem.RestarItem, error) // pagination?
+	GetByID(id int) (*restaritem.RestarItem, error)
+}
+
+func NewHTTPRestaritemDelivery(uc IHTTPRestaritemUsecase) *HTTPRestaritemDelivery {
+	return &HTTPRestaritemDelivery{
+		uc: uc,
+	}
+}
+
+type HTTPRestaritemDelivery struct {
+	uc IHTTPRestaritemUsecase
+}
+
+// 1: создать новый итем, возвращает id итема, который потом надо перенаправить в qr код
+func (h *HTTPRestaritemDelivery) Create(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// 2: получить все итемы
+func (h *HTTPRestaritemDelivery) GetAll(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// 3: получить итем по id
+func (h *HTTPRestaritemDelivery) GetByID(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// 4: страница с данными об итеме, включая дефекты, работы, фото
+func (h *HTTPRestaritemDelivery) RestaritemView(w http.ResponseWriter, r *http.Request) {
+
+}
